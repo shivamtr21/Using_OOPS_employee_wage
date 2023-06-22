3880963d6272ca61f067e605e5eb8d3ac5bc6dac
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-22 07:59:28 +0530

    [shivam tripathi]: added Employee Wage problem

---
+++ EmpWage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_OOPS_Employee_Wage
+{
+    public class EmpWage
+    {
+        public void Wage()
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int result = random.Next(2);
+
+
+            if (result == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Using_OOPS_Employee_Wage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWage ep = new EmpWage();
+            ep.Wage();
+        }
+    }
+}
