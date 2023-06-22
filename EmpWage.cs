860b4730053761e2c3b9ca920cb6e9e3b2d0d05e
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-22 08:10:02 +0530

    [shivam tripathi]: added Employee Wage uc-2 problem

--- EmpWage.cs
+++ EmpWage.cs
@@ -11,6 +11,11 @@ namespace Using_OOPS_Employee_Wage
         public void Wage()
         {
             int IS_FULL_TIME = 1;
+            int EMP_WAGE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
             Random random = new Random();
 
             int result = random.Next(2);
@@ -19,11 +24,18 @@ namespace Using_OOPS_Employee_Wage
             if (result == IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
+
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_WAGE_PER_HOUR;
+
+            Console.WriteLine("Emp Wage : " + empWage);
 
         }
     }
