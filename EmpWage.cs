8b46346d0ff659f14aad74082bb81600d1c41154
shivamtr21 <shivamtripathi26297@gmail.com>
2023-06-22 14:13:40 +0530

    [shivam tripathi]: added Employee Wage uc-6 problem

--- EmpWage.cs
+++ EmpWage.cs
@@ -15,47 +15,52 @@ namespace Using_OOPS_Employee_Wage
         public const int NUM_OF_WORKING_DAYS = 20;
         public void Wage()
         {
-           
 
-            int empHrs = 0;
-            int empWage = 0;
-            int totalEmpWage = 0;
 
-            for(int day = 0; day <NUM_OF_WORKING_DAYS; day++)
+            int Max_Days = 20;
+            int Max_Hours = 100;
+            int Wage_Per_hrs = 20;
+            int EmpHrs = 0;
+
+            int Total_Wage = 0;
+
+            int Total_Work_Hour = 0;
+            int day = 1;
+
+            while (day <= Max_Days && Total_Work_Hour <= Max_Hours)
             {
 
-
                 Random random = new Random();
+                int num = random.Next(0, 3);
 
-                int result = random.Next(3);
-
-
-                switch (result)
+                switch (num)
                 {
-                    case IS_PART_TIME:
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        EmpHrs = 0;
+                        break;
+
+                    case 1:
                         Console.WriteLine("Employee is present for part time");
-                        empHrs = 4;
+                        EmpHrs = 4;
                         break;
 
-                    case IS_FULL_TIME:
+                    case 2:
                         Console.WriteLine("Employee is present for full time");
-                        empHrs = 8;
-                        break;
-
-                    default:
-                        Console.WriteLine("Employee is absent");
-                        empHrs = 0;
+                        EmpHrs = 8;
                         break;
                 }
+                day++;
+                Total_Work_Hour = (Total_Work_Hour + EmpHrs);
 
-                empWage = empHrs * EMP_WAGE_PER_HOUR;
-                totalEmpWage += empWage;
-
-                Console.WriteLine("Emp Wage : " + empWage);
-
+                Total_Wage = (Total_Work_Hour * Wage_Per_hrs);
             }
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
- 
+
+            Console.WriteLine("Total working hour in a Month " + Total_Work_Hour);
+
+            Console.WriteLine("Total salary for a month  " + Total_Wage);
+
+
 
 
 
